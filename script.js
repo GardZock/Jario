c9469b422f6029cc0ab21485841f5c13524059a4
GardZock <79826985+GardZock@users.noreply.github.com>
2024-03-14 08:37:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const mario = document.querySelector('.jario');
+const pipe = document.querySelector('.cano');
+
+
+const jump = () => {
+    mario.classList.add('pulo');
+    setTimeout(() => {
+        mario.classList.remove('pulo');
+    }, 500);
+}
+
+const loop = setInterval(() => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 88) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './images/game-over.png';
+        mario.style.width = '75px';
+        mario.style.marginLeft = '50px';
+
+        clearInterval(loop);
+    }
+}, 10);
+
+document.addEventListener('keydown', jump);
